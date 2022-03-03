0774b8f67aea39ef558b37950e0e0485438c8c6f
Unique Orduna <uniqueorduna@swuser-129-8-206-99.swlan.csufresno.edu>
2022-03-03 12:01:58 -0800

    adding

---
+++ class_average.py
@@ -0,0 +1,23 @@
+
+
+"""
+input: the test scores 
+initiate a iterator (counter) and accumulator (sum) and set it to zero 
+loop through the list of test scores
+add all the test scores
+increment the counter by one
+once the loop ends, there are no more scores to add
+divide the accumulator (sum) by the counter 
+display the output to the user
+output: print the average of the class scores
+"""
+
+"""
+scores
+iterator, accumulator = 0
+loop through scores
+    accumulator = accumulator + scores 
+    iterator = iterator + 1 
+output = accumulator / total score
+print output
+"""
